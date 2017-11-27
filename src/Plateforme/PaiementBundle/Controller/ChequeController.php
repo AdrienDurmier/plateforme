9cf1927fe8e2b9dd20b2d79fa056d90306d18c62
AdrienDurmier <a.durmier@gmail.com>
2017-11-27 22:18:32 +0100

    Ecommerce : paiement par cheque

--- src/Plateforme/PaiementBundle/Controller/ChequeController.php
+++ src/Plateforme/PaiementBundle/Controller/ChequeController.php
@@ -6,13 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class ChequeController extends Controller {
-  
-  
+
   /**
    * Formulaire de paiement
    */
-  public function formAction() {
+  public function formAction(Request $request) {
+    $em = $this->getDoctrine()->getManager();
+
+    $site_name = $em->getRepository('PlateformeCoreBundle:Variable')->findOneByCode('site_name');
+    $adresse_voie = $em->getRepository('PlateformeCoreBundle:Variable')->findOneByCode('adresse_voie');
+    $adresse_complement = $em->getRepository('PlateformeCoreBundle:Variable')->findOneByCode('adresse_complement');
+    $adresse_code_postal = $em->getRepository('PlateformeCoreBundle:Variable')->findOneByCode('adresse_code_postal');
+    $adresse_commune = $em->getRepository('PlateformeCoreBundle:Variable')->findOneByCode('adresse_commune');
+    $adresse_pays = $em->getRepository('PlateformeCoreBundle:Variable')->findOneByCode('adresse_pays');
+
+
     return $this->render('PlateformePaiementBundle:Cheque:form.html.twig', array(
+          'site_name'             => $site_name->getValue(),
+          'adresse_voie'          => $adresse_voie->getValue(),
+          'adresse_complement'    => $adresse_complement->getValue(),
+          'adresse_code_postal'   => $adresse_code_postal->getValue(),
+          'adresse_commune'       => $adresse_commune->getValue(),
+          'adresse_pays'          => $adresse_pays->getValue(),
     ));
   }
 
@@ -22,19 +37,19 @@ class ChequeController extends Controller {
   public function returnAction(Request $request) {
     $em = $this->getDoctrine()->getManager();
     $valeurs_recu = $request->request->all();
-    
+
     // Récupération des status
     $statut_a_livrer = $em->getRepository('PlateformeEcommerceBundle:Statut')->find(6);
-    
+
     // Modification du statut de la commande
     $commande = $em->getRepository('PlateformeEcommerceBundle:Commande')->find($valeurs_recu['commande_id']);
     $commande->setStatut($statut_a_livrer);
     $em->persist($commande);
     $em->flush();
-    
+
     $session = $request->getSession();
     $session->remove('panier');
     return $this->render('PlateformeEcommerceBundle:PaiementCheque:retour.html.twig');
   }
-  
+
 }
