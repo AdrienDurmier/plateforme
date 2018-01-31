1c4b7620fa040f92fce7fa9a11ea1830b8fd8a16
AdrienDurmier <a.durmier@gmail.com>
2018-01-31 22:09:55 +0100

    test contributions sur version originale ok (page standard)

--- src/Plateforme/CoreBundle/Controller/PageController.php
+++ src/Plateforme/CoreBundle/Controller/PageController.php
@@ -5,6 +5,8 @@ namespace Plateforme\CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Plateforme\CoreBundle\Entity\Contribution;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PageController extends Controller {
 
@@ -18,14 +20,14 @@ class PageController extends Controller {
           'pages' => $pages,
     ));
   }
-  
+
   /**
    * Publie ou dépublie une page
    */
   public function publicationAction(Request $request) {
     $em = $this->getDoctrine()->getManager();
     $valeurs_recu = $request->request->all();
-    
+
     $page = $em->getRepository('PlateformeCoreBundle:Page')->find($valeurs_recu['page_id']);
     if (null === $page) {
       throw new NotFoundHttpException("La page ayant l'identifiant " . $valeurs_recu['page_id'] . " n'existe pas.");
@@ -33,12 +35,64 @@ class PageController extends Controller {
     $page->setIsPublic(!$valeurs_recu['page_is_public']);
     $em->persist($page);
     $em->flush();
-    if($valeurs_recu['page_is_public']){
+    if ($valeurs_recu['page_is_public']) {
       $request->getSession()->getFlashBag()->add('warning', "Page dépubliée.");
-    }else{
+    }
+    else {
       $request->getSession()->getFlashBag()->add('success', "Page publiée.");
     }
     return $this->redirect($request->server->get('HTTP_REFERER'));
+  }
+
+  /**
+   * Ajoute un commentaire à une version
+   */
+  public function addVersionCommentAction(Request $request) {
+    $em = $this->getDoctrine()->getManager();
+    $valeurs_recu = $request->request->all();
+    $page = $em->getRepository('PlateformeCoreBundle:Page')->find($valeurs_recu['version_id']);
+    if (null === $page) {
+      throw new NotFoundHttpException("La page ayant l'identifiant " . $valeurs_recu['version_id'] . " n'existe pas.");
+    }
+    $contribution = new Contribution();
+    $contribution->setPage($page);
+    $contribution->setUser($this->getUser());
+    $contribution->setComment($valeurs_recu['version_comment']);
+    $em->persist($contribution);
+    $em->flush();
+
+    return $this->redirect($request->server->get('HTTP_REFERER'));
+  }
+
+  /**
+   * Récupère tous les commentaires d'une version
+   */
+  public function getVersionCommentsAction(Request $request) {
+    $status = true;
+    $em = $this->getDoctrine()->getManager();
+    $valeurs_recu = $request->request->all();
+    $page = $em->getRepository('PlateformeCoreBundle:Page')->find($valeurs_recu['version_id']);
+    $contributions = $em->getRepository('PlateformeCoreBundle:Contribution')->findByPage($page);
+    $resultContributions = [];
+    foreach ($contributions as $contribution) {
+      $resultContributions[] = array(
+        'id' => $contribution->getId(),
+        'user' => $contribution->getUser()->getUsername(),
+        'comment' => $contribution->getComment(),
+        'date' => date_format($contribution->getCreated(),"d/m/Y \à H:i:s"),
+      );
+    }
+    // S'il n'y a eu aucun résultat
+    if (empty($resultContributions)) {
+      $status = false;
+    }
+    $response = array(
+      'success' => $status,
+      'data' => array(
+        'contributions' => $resultContributions
+      )
+    );
+    return new JsonResponse($response);
   }
 
 }
